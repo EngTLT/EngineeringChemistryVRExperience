664d1cd867bc03495b6bfcdd7e53435c782742a8
Daniel Clarke <d.clarke@queensu.ca>
2018-11-02 11:59:43 -0400

    Adding narration to everything.

--- Assets/Scripts/NarrationHandler.cs
+++ Assets/Scripts/NarrationHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class NarrationHandler : MonoBehaviour {
 
     public static NarrationHandler instance;
